ed63efa267d402e1c256994544632b79e2a27e80
nikarpenko1 <nikarpenko@edu.hse.ru>
2024-05-19 11:36:58 +0300

    QT linguist and README added

--- Application/build/Desktop_x86_windows_msvc2005_pe_32bit-Debug/qtcsettings.cmake
+++ Application/build/Desktop_x86_windows_msvc2005_pe_32bit-Debug/qtcsettings.cmake
@@ -1,10 +1,2 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Coding space/MIOIT/Application/build/Desktop_x86_windows_msvc2005_pe_32bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "C:/vcpkg/installed/x64-windows/tools/Qt6/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "C:/vcpkg/installed/x64-windows" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.39.33519/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.39.33519/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
---
+++ README.md
@@ -0,0 +1,10 @@
+#Installation guide for vcpkg
+
+If you're missing vcpkg installed,
+look no further than https://vcpkg.io/en/index.html
+
+Firstly, install QT basic pack
+vcpkg install qtbase
+
+Then, you will need QTlinguist so
+vcpkg install qttools
